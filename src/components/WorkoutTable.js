67cf17349414f7a472fa6f17ae099726bcde1a14
Luke Lai <luke@lukeelai.com>
2019-12-18 08:45:32 -0800

    create table data component

--- src/components/WorkoutTable.js
+++ src/components/WorkoutTable.js
@@ -1,5 +1,8 @@
 import React from "react";
-import { Table, Input, Button } from "reactstrap";
+import { Table, Button } from "reactstrap";
+
+//Components
+import WorkoutTableData from "./WorkoutTableData";
 
 export const WorkoutTable = props => {
   return (
@@ -14,35 +17,7 @@ export const WorkoutTable = props => {
         </tr>
       </thead>
       {props.workout.map(workout => {
-        return (
-          <tbody>
-            <tr>
-              <th>{workout.T1 ? workout.T1 : "T1"} - 5x3</th>
-              <td>
-                <Input
-                  value={workout.T1_Weight}
-                  id="t1-weight"
-                  plaintext={workout.plaintext}
-                  onClick={props.onClickEdit}
-                  onChange={props.onChangeWeight}
-                  onKeyDown={props.onKeyDown}
-                  readOnly={workout.readonly}
-                />
-              </td>
-              <td>{workout.T1_Last}</td>
-            </tr>
-            <tr>
-              <th>Bench - 3x10</th>
-              <td id="t2">37.5</td>
-              <td>Fail</td>
-            </tr>
-            <tr>
-              <th>Accessory - 3x10+</th>
-              <td id="t3">85</td>
-              <td>20</td>
-            </tr>
-          </tbody>
-        );
+        return <WorkoutTableData {...workout} />;
       })}
     </Table>
   );
---
+++ src/components/WorkoutTableData.js
@@ -0,0 +1,37 @@
+import React from "react";
+import { Input } from "reactstrap";
+
+export const WorkoutTableData = props => {
+  console.log(props);
+  return (
+    <tbody>
+      <tr>
+        <th>{props.T1 ? props.T1 : "T1"} - 5x3</th>
+        <td>
+          <Input
+            value={props.T1_Weight}
+            id="t1-weight"
+            plaintext={props.plaintext}
+            onClick={props.onClickEdit}
+            onChange={props.onChangeWeight}
+            onKeyDown={props.onKeyDown}
+            readOnly={props.readonly}
+          />
+        </td>
+        <td>{props.T1_Last}</td>
+      </tr>
+      <tr>
+        <th>Bench - 3x10</th>
+        <td id="t2">37.5</td>
+        <td>Fail</td>
+      </tr>
+      <tr>
+        <th>Accessory - 3x10+</th>
+        <td id="t3">85</td>
+        <td>20</td>
+      </tr>
+    </tbody>
+  );
+};
+
+export default WorkoutTableData;
