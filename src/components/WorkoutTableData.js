3aae565e9bd63c4d507703cb4acf0bf96a9a0fe7
Luke Lai <luke@lukeelai.com>
2019-12-18 09:19:19 -0800

    map table data only if date has values

--- src/components/WorkoutTableData.js
+++ src/components/WorkoutTableData.js
@@ -3,32 +3,32 @@ import { Input } from "reactstrap";
 
 export const WorkoutTableData = props => {
   const date = props.date.replace(/\//g, "");
-  console.log(props[date]);
-  return (
-    <tbody>
-      {Object.keys(props[12162019]).map(key => {
-        return (
-          <tr>
-            <th>
-              {props[12162019][key] ? props[12162019][key][0] : "T1"} - 5 x 3
-            </th>
-            <td>
-              <Input
-                value={props[12162019][key][1]}
-                id={key + "-weight"}
-                plaintext={props[12162019][key][3]}
-                onClick={props.onClick}
-                onChange={props.onChange}
-                onKeyDown={props.onKeyDown}
-                readOnly={props[12162019][key][4]}
-              />
-            </td>
-            <td>{props[12162019][key][2]}</td>
-          </tr>
-        );
-      })}
-    </tbody>
-  );
+  console.log();
+  if (props[date])
+    return (
+      <tbody>
+        {Object.keys(props[date]).map(key => {
+          return (
+            <tr>
+              <th>{props[date][key] ? props[date][key][0] : "T1"} - 5 x 3</th>
+              <td>
+                <Input
+                  value={props[date][key][1]}
+                  id={key + "-weight"}
+                  plaintext={props[date][key][3]}
+                  onClick={props.onClick}
+                  onChange={props.onChange}
+                  onKeyDown={props.onKeyDown}
+                  readOnly={props[date][key][4]}
+                />
+              </td>
+              <td>{props[date][key][2]}</td>
+            </tr>
+          );
+        })}
+      </tbody>
+    );
+  return <tbody></tbody>;
 };
 
 export default WorkoutTableData;
